1933719802387fc4f0d70fc4ff50039dd3745db2
Rohan kumar Padhan <82263270+Mrrohan23@users.noreply.github.com>
2025-04-17 12:10:58 +0530

    Update main.cpp

--- cpp/examples/outstation/main.cpp
+++ cpp/examples/outstation/main.cpp
@@ -1,87 +1,107 @@
-#include <opendnp3/outstation/IOutstationApplication.h>
-#include <opendnp3/outstation/OutstationStackConfig.h>
-#include <opendnp3/outstation/UpdateBuilder.h>
-#include <opendnp3/outstation/OutstationConfig.h>
-#include <opendnp3/outstation/DatabaseConfig.h>
-
-#include <asiodnp3/DefaultOutstationApplication.h>
-#include <asiodnp3/UpdateHandler.h>
-#include <asiodnp3/OutstationStack.h>
-#include <asiodnp3/DNP3Manager.h>
-
-#include <boost/asio.hpp>
 #include <iostream>
 #include <string>
 #include <thread>
-#include <chrono>
+#include <boost/asio.hpp>
 
+#include <asiodnp3/DNP3Manager.h>
+#include <asiodnp3/PrintingChannelListener.h>
+#include <asiodnp3/ConsoleLogger.h>
+#include <asiodnp3/UpdateBuilder.h>
+#include <asiodnp3/DefaultOutstationApplication.h>
+#include <asiodnp3/TLSConfig.h>
+
+#include <opendnp3/outstation/SimpleCommandHandler.h>
+#include <opendnp3/LogLevels.h>
+#include <opendnp3/outstation/DatabaseConfig.h>
+#include <opendnp3/outstation/EventBufferConfig.h>
+
+using namespace std;
+using namespace boost::asio;
 using namespace opendnp3;
 using namespace asiodnp3;
-using boost::asio::ip::tcp;
 
-void start_sensor_listener(IOutstation& outstation) {
-    std::thread([](IOutstation& outstation) {
-        try {
-            boost::asio::io_context io_context;
-            tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 20000));
-            std::cout << "[INFO] Listening for sensor data on TCP port 20000...\n";
+void handle_sensor_data(const std::string& data, std::shared_ptr<IOutstation> outstation)
+{
+    float temperature = 0, pressure = 0, humidity = 0;
+    sscanf(data.c_str(), "%f,%f,%f", &temperature, &pressure, &humidity);
 
-            while (true) {
-                tcp::socket socket(io_context);
-                acceptor.accept(socket);
-                std::cout << "[INFO] Sensor connected.\n";
+    UpdateBuilder builder;
+    builder.Update(Analog(temperature), 0);
+    builder.Update(Analog(pressure), 1);
+    builder.Update(Analog(humidity), 2);
 
-                char data[1024];
-                size_t length = socket.read_some(boost::asio::buffer(data));
-                data[length] = '\0';
-                std::string message(data);
-
-                std::cout << "[RECEIVED] " << message << "\n";
-
-                // Parse "temp,pressure,humidity"
-                float temp, pressure, humidity;
-                if (sscanf(message.c_str(), "%f,%f,%f", &temp, &pressure, &humidity) == 3) {
-                    UpdateBuilder builder;
-                    builder.Update(Binary(true, Timestamp::Min()), 0); // Dummy binary status
-                    builder.Update(Analog(temp), 1);
-                    builder.Update(Analog(pressure), 2);
-                    builder.Update(Analog(humidity), 3);
-                    outstation.Apply(builder.Build());
-                    std::cout << "[INFO] Data applied to DNP3 database.\n";
-                }
-
-                socket.close();
-            }
-        } catch (const std::exception& e) {
-            std::cerr << "[ERROR] Sensor listener: " << e.what() << "\n";
-        }
-    }, std::ref(outstation)).detach();
+    outstation->Apply(builder.Build());
 }
 
-int main() {
-    std::cout << "[INFO] Starting DNP3 Outstation...\n";
+void start_python_data_server(std::shared_ptr<IOutstation> outstation)
+{
+    try {
+        boost::asio::io_context io;
+        ip::tcp::acceptor acceptor(io, ip::tcp::endpoint(ip::tcp::v4(), 15000));
+        std::cout << "[INFO] Listening for sensor data on port 15000..." << std::endl;
 
-    DNP3Manager manager(1, [](const std::string& msg) { std::cout << msg << "\n"; });
+        while (true) {
+            ip::tcp::socket socket(io);
+            acceptor.accept(socket);
+            std::cout << "[INFO] Sensor data client connected." << std::endl;
 
-    auto channel = manager.AddTCPServer("server", levels::NORMAL, ServerAcceptMode::CloseOld,
-                                        IPEndpoint("0.0.0.0", 20001), nullptr);
+            char data[1024];
+            size_t length = socket.read_some(boost::asio::buffer(data));
+            data[length] = '\0';
+            std::string received_data(data);
 
-    OutstationStackConfig config(DatabaseConfig(10, 10));
-    config.outstation.eventBufferConfig = EventBufferConfig(10);
-    config.outstation.params.allowUnsolicited = true;
+            std::cout << "[DATA RECEIVED] " << received_data << std::endl;
+            handle_sensor_data(received_data, outstation);
+
+            socket.close();
+        }
+
+    } catch (const std::exception& e) {
+        std::cerr << "[ERROR] Sensor server failed: " << e.what() << std::endl;
+    }
+}
+
+int main()
+{
+    std::cout << "[INFO] Starting TLS-enabled DNP3 Outstation..." << std::endl;
+
+    const uint32_t FILTERS = levels::NORMAL | levels::ALL_COMMS;
+    DNP3Manager manager(1, ConsoleLogger::Create());
+
+    TLSConfig tlsConfig(
+        "server-cert.pem",  // Server certificate
+        "server-key.pem",   // Server private key
+        "ca-cert.pem",      // Trusted CA cert
+        CertificateMode::VerifyIfPresent
+    );
+
+    auto channel = manager.AddTLSServer(
+        "tls-server",
+        FILTERS,
+        ChannelRetry::Default(),
+        tlsConfig,
+        "0.0.0.0",
+        20000,
+        PrintingChannelListener::Create()
+    );
+
+    OutstationStackConfig config(DatabaseSizes::AllTypes(10));
     config.link.LocalAddr = 10;
     config.link.RemoteAddr = 1;
+    config.outstation.eventBufferConfig = EventBufferConfig::AllTypes(10);
+    config.outstation.params.allowUnsolicited = true;
 
-    auto outstation = channel->AddOutstation("outstation",
-                    [](const OutstationConfig&) { return std::make_shared<UpdateHandler>(); },
-                    DefaultOutstationApplication::Create(), config);
+    auto outstation = channel->AddOutstation(
+        "outstation",
+        SuccessCommandHandler::Create(),
+        DefaultOutstationApplication::Create(),
+        config
+    );
 
     outstation->Enable();
 
-    // Start listening for data from the Python script
-    start_sensor_listener(*outstation);
+    std::thread sensor_thread(start_python_data_server, outstation);
+    sensor_thread.join();
 
-    // Keep main thread alive
-    std::this_thread::sleep_for(std::chrono::hours(24));
     return 0;
 }
