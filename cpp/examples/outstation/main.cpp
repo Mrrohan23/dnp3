c171f23c25a1de7949900698df9769dbce08eea4
Rohan kumar Padhan <82263270+Mrrohan23@users.noreply.github.com>
2025-04-17 14:29:40 +0530

    Update main.cpp

--- cpp/examples/outstation/main.cpp
+++ cpp/examples/outstation/main.cpp
@@ -1,104 +1,115 @@
-#include <iostream>
+#include <asiodnp3/DNP3Manager.h>
+#include <asiodnp3/PrintingSOEHandler.h>
+#include <asiodnp3/PrintingChannelListener.h>
+#include <asiodnp3/ConsoleLogger.h>
+#include <asiodnp3/UpdateBuilder.h>
+
+#include <asiopal/UTCTimeSource.h>
+#include <opendnp3/outstation/SimpleCommandHandler.h>
+
+#include <opendnp3/outstation/IUpdateHandler.h>
+
+#include <opendnp3/LogLevels.h>
+
 #include <string>
 #include <thread>
+#include <iostream>
 #include <boost/asio.hpp>
-
-#include <asiodnp3/DNP3Manager.h>
-#include <asiodnp3/PrintingChannelListener.h>
-#include <asiodnp3/UpdateBuilder.h>
-#include <opendnp3/outstation/SimpleCommandHandler.h>
-#include <opendnp3/outstation/IOutstationApplication.h>
+#include <sstream>
 
 using namespace std;
-using namespace boost::asio;
-using namespace asiodnp3;
 using namespace opendnp3;
+using namespace openpal;
+using namespace asiopal;
+using namespace asiodnp3;
+using boost::asio::ip::tcp;
 
-// Minimal application, only what's supported
-class BasicOutstationApp : public IOutstationApplication {
-public:
-    RestartMode ColdRestartSupport() const override { return RestartMode::UNSUPPORTED; }
-    RestartMode WarmRestartSupport() const override { return RestartMode::UNSUPPORTED; }
-    uint16_t ColdRestart() override { return 0; }
-    uint16_t WarmRestart() override { return 0; }
-};
+void ConfigureDatabase(DatabaseConfig& config)
+{
+	config.analog[0].clazz = PointClass::Class2;
+	config.analog[0].svariation = StaticAnalogVariation::Group30Var5;
+	config.analog[0].evariation = EventAnalogVariation::Group32Var7;
 
-// Handles sensor input like "24.5,1012.0,55.2"
-void handle_sensor_data(const std::string& data, std::shared_ptr<IOutstation> outstation) {
-    float temp, pressure, humidity;
-    if (sscanf(data.c_str(), "%f,%f,%f", &temp, &pressure, &humidity) == 3) {
-        UpdateBuilder builder;
-        builder.Update(Analog(temp), 0);
-        builder.Update(Analog(pressure), 1);
-        builder.Update(Analog(humidity), 2);
-        outstation->Apply(builder.Build());
-
-        std::cout << "[UPDATED] T=" << temp << ", P=" << pressure << ", H=" << humidity << std::endl;
-    } else {
-        std::cerr << "[ERROR] Invalid format: " << data << std::endl;
-    }
+	config.analog[1].clazz = PointClass::Class2;
+	config.analog[2].clazz = PointClass::Class2;
 }
 
-// TCP server to receive Python data
-void start_python_server(std::shared_ptr<IOutstation> outstation) {
-    try {
-        io_context io;
-        ip::tcp::acceptor acceptor(io, ip::tcp::endpoint(ip::tcp::v4(), 15000));
-        std::cout << "[INFO] Waiting for Python data on port 15000..." << std::endl;
+void ReceiveAndSendSensorData(std::shared_ptr<IOutstation> outstation)
+{
+	try
+	{
+		boost::asio::io_context io_context;
+		tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 15000));
+		std::cout << "[INFO] Listening for sensor data on port 15000..." << std::endl;
 
-        while (true) {
-            ip::tcp::socket socket(io);
-            acceptor.accept(socket);
+		while (true)
+		{
+			tcp::socket socket(io_context);
+			acceptor.accept(socket);
+			std::cout << "[INFO] Client connected to send sensor data." << std::endl;
 
-            char data[1024] = {0};
-            size_t len = socket.read_some(buffer(data));
-            data[len] = '\0';
+			char buffer[1024];
+			size_t length = socket.read_some(boost::asio::buffer(buffer));
+			buffer[length] = '\0';
 
-            std::string received(data);
-            std::cout << "[RECEIVED] " << received << std::endl;
-            handle_sensor_data(received, outstation);
+			std::string data(buffer);
+			std::cout << "[DATA RECEIVED] " << data << std::endl;
 
-            socket.close();
-        }
-    } catch (const std::exception& e) {
-        std::cerr << "[ERROR] TCP Server: " << e.what() << std::endl;
-    }
+			std::istringstream iss(data);
+			std::string tempStr, pressStr, humidStr;
+
+			if (std::getline(iss, tempStr, ',') &&
+				std::getline(iss, pressStr, ',') &&
+				std::getline(iss, humidStr, ','))
+			{
+				float temperature = std::stof(tempStr);
+				float pressure = std::stof(pressStr);
+				float humidity = std::stof(humidStr);
+
+				UpdateBuilder builder;
+				builder.Update(Analog(temperature), 0); // Index 0
+				builder.Update(Analog(pressure), 1);    // Index 1
+				builder.Update(Analog(humidity), 2);    // Index 2
+
+				outstation->Apply(builder.Build());
+				std::cout << "[INFO] Sent to outstation: T=" << temperature
+					  << ", P=" << pressure << ", H=" << humidity << std::endl;
+			}
+			else
+			{
+				std::cerr << "[ERROR] Invalid data format received: " << data << std::endl;
+			}
+
+			socket.close();
+		}
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "[ERROR] Exception in ReceiveAndSendSensorData: " << e.what() << std::endl;
+	}
 }
 
-int main() {
-    std::cout << "[START] DNP3 Outstation" << std::endl;
+int main(int argc, char* argv[])
+{
+	const uint32_t FILTERS = levels::NORMAL | levels::ALL_COMMS;
+	DNP3Manager manager(1, ConsoleLogger::Create());
 
-    DNP3Manager manager(1);
+	auto channel = manager.AddTCPServer("server", FILTERS, ChannelRetry::Default(), "0.0.0.0", 20000, PrintingChannelListener::Create());
 
-    // Fallback retry config (if ChannelRetry not available)
-    auto retry = openpal::TimeDuration::Seconds(5);
+	OutstationStackConfig config(DatabaseSizes::AllTypes(10));
+	config.outstation.eventBufferConfig = EventBufferConfig::AllTypes(10);
+	config.outstation.params.allowUnsolicited = true;
+	config.link.LocalAddr = 10;
+	config.link.RemoteAddr = 1;
+	config.link.KeepAliveTimeout = openpal::TimeDuration::Max();
 
-    auto channel = manager.AddTCPServer(
-        "server",
-        0,  // No log filters
-        retry,
-        "0.0.0.0",
-        20000,
-        PrintingChannelListener::Create()
-    );
+	ConfigureDatabase(config.dbConfig);
 
-    OutstationStackConfig config(DatabaseSizes::AllTypes(10));
-    config.link.LocalAddr = 10;
-    config.link.RemoteAddr = 1;
-    config.outstation.eventBufferConfig = EventBufferConfig::AllTypes(10);
-    config.outstation.params.allowUnsolicited = true;
+	auto outstation = channel->AddOutstation("outstation", SuccessCommandHandler::Create(), DefaultOutstationApplication::Create(), config);
+	outstation->Enable();
 
-    auto outstation = channel->AddOutstation(
-        "outstation",
-        SuccessCommandHandler::Create(),
-        std::make_shared<BasicOutstationApp>(),
-        config
-    );
+	std::thread sensorThread(ReceiveAndSendSensorData, outstation);
+	sensorThread.join();
 
-    outstation->Enable();
-
-    std::thread listener(start_python_server, outstation);
-    listener.join();
-
-    return 0;
+	return 0;
 }
