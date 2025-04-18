886039892465aa2b53c083823627dd26c6f8b7cb
Rohan kumar Padhan <82263270+Mrrohan23@users.noreply.github.com>
2025-04-18 17:20:41 +0530

    Update main.cpp

--- cpp/examples/outstation/main.cpp
+++ cpp/examples/outstation/main.cpp
@@ -22,6 +22,13 @@ using namespace asiopal;
 using namespace asiodnp3;
 using boost::asio::ip::tcp;
 
+struct State {
+    uint32_t count = 0;
+    double value = 0;
+    bool binary = false;
+    DoubleBit dbit = DoubleBit::DETERMINED_OFF;
+};
+
 void ConfigureDatabase(DatabaseConfig& config)
 {
     config.analog[0].clazz = PointClass::Class1;
@@ -32,6 +39,34 @@ void ConfigureDatabase(DatabaseConfig& config)
     config.analog[2].clazz = PointClass::Class1;
 
     config.binary[0].clazz = PointClass::Class1;
+}
+
+void AddUpdates(UpdateBuilder& builder, State& state, const std::string& arguments)
+{
+    for (const char& c : arguments)
+    {
+        switch (c)
+        {
+        case 'c':
+            builder.Update(Counter(state.count), 0);
+            ++state.count;
+            break;
+        case 'a':
+            builder.Update(Analog(state.value), 0);
+            state.value += 1;
+            break;
+        case 'b':
+            builder.Update(Binary(state.binary), 0);
+            state.binary = !state.binary;
+            break;
+        case 'd':
+            builder.Update(DoubleBitBinary(state.dbit), 0);
+            state.dbit = (state.dbit == DoubleBit::DETERMINED_OFF) ? DoubleBit::DETERMINED_ON : DoubleBit::DETERMINED_OFF;
+            break;
+        default:
+            break;
+        }
+    }
 }
 
 void ReceiveSensorData(std::shared_ptr<IOutstation> outstation)
@@ -64,19 +99,18 @@ void ReceiveSensorData(std::shared_ptr<IOutstation> outstation)
                 float temperature = std::stof(tempStr);
                 float pressure = std::stof(pressStr);
                 float humidity = std::stof(humidStr);
-                bool binary = (binaryStr == "1");
+                bool binaryValue = (binaryStr == "1");
 
                 UpdateBuilder builder;
                 builder.Update(Analog(temperature), 0);
                 builder.Update(Analog(pressure), 1);
                 builder.Update(Analog(humidity), 2);
-                builder.Update(Binary(binary), 0); // binary point 0
-
+                builder.Update(Binary(binaryValue), 0);
                 outstation->Apply(builder.Build());
 
                 std::cout << "[INFO] Sent to outstation: T=" << temperature
                           << ", P=" << pressure << ", H=" << humidity
-                          << ", Binary=" << binary << std::endl;
+                          << ", Binary=" << binaryValue << std::endl;
             }
             else
             {
@@ -95,20 +129,17 @@ void ReceiveSensorData(std::shared_ptr<IOutstation> outstation)
 void HandleUserInput(std::shared_ptr<IOutstation> outstation)
 {
     string input;
+    State state;
     while (true)
     {
-        std::cout << "Manual input (format: a=analog, b=binary, quit to exit): ";
+        std::cout << "Enter one or more measurement changes then press <enter>" << std::endl;
+        std::cout << "c = counter, b = binary, d = doublebit, a = analog, 'quit' = exit" << std::endl;
         std::cin >> input;
 
-        if (input == "quit") break;
+        if (input == "quit") exit(0);
 
         UpdateBuilder builder;
-        if (input == "a") {
-            builder.Update(Analog(42.0), 0);
-        } else if (input == "b") {
-            builder.Update(Binary(true), 0);
-        }
-
+        AddUpdates(builder, state, input);
         outstation->Apply(builder.Build());
     }
 }
@@ -118,33 +149,19 @@ int main(int argc, char* argv[])
     const uint32_t FILTERS = levels::NORMAL | levels::ALL_COMMS;
     DNP3Manager manager(1, ConsoleLogger::Create());
 
-    auto channel = manager.AddTCPServer(
-        "server",
-        FILTERS,
-        ChannelRetry::Default(),
-        "0.0.0.0",
-        20000,
-        PrintingChannelListener::Create()
-    );
+    auto channel = manager.AddTCPServer("server", FILTERS, ChannelRetry::Default(), "0.0.0.0", 20000, PrintingChannelListener::Create());
 
     OutstationStackConfig config(DatabaseSizes::AllTypes(10));
-    config.outstation.eventBufferConfig = EventBufferConfig::AllTypes(10); // enable historical buffering
-    config.outstation.params.allowUnsolicited = true;                      // allow unsolicited
+    config.outstation.eventBufferConfig = EventBufferConfig::AllTypes(10);
+    config.outstation.params.allowUnsolicited = true; // ✅ enable unsolicited messaging
     config.link.LocalAddr = 10;
     config.link.RemoteAddr = 1;
     config.link.KeepAliveTimeout = openpal::TimeDuration::Max();
 
     ConfigureDatabase(config.dbConfig);
 
-    auto outstation = channel->AddOutstation(
-        "outstation",
-        SuccessCommandHandler::Create(),
-        DefaultOutstationApplication::Create(),
-        config
-    );
-
-    outstation->Enable();
-    outstation->EnableUnsolicited(); // 🔥 actively push updates
+    auto outstation = channel->AddOutstation("outstation", SuccessCommandHandler::Create(), DefaultOutstationApplication::Create(), config);
+    outstation->Enable(); // ✅ enable outstation
 
     std::thread sensorThread(ReceiveSensorData, outstation);
     std::thread inputThread(HandleUserInput, outstation);
